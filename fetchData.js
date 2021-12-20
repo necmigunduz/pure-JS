fd524cdf301b3eea6b70ab93399ec8d63cfb54cb
necmigunduz <necmigunduz@gmail.com>
2021-12-20 13:43:44 +0300

    Place products on the page

--- fetchData.js
+++ fetchData.js
@@ -1,26 +1,20 @@
-let products = []
-let html = ``
-
 async function fetchData(){
     await fetch("./product-list.json")
         .then(response => {
             return response.json()
         })
         .then(data => {
-            // console.log(data.responses[0][0].params.recommendedProducts)
-            products.push(data.responses[0][0].params.recommendedProducts)
-            
-            let html = `${products[0]['Size Özel'][0].name}`
-            
+            let products = data.responses[0][0].params.recommendedProducts['Size Özel']
+            console.log(products)
+            const html = products.map(product => {
+                return `<p>${product.name}</p>`
+            })
+
             console.log(html)
-            document.querySelector('#main').innerHTML = `
-            <h2>Special Product</h2>
-            <span>${html}</span>
-            `
         })
         .catch(error => {
             console.log(error)
         })
 }
 
-export { fetchData, products, html };
+export { fetchData };
--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-import { fetchData, products, html } from './fetchData.js'
+async function fetchData(){
+    await fetch("./product-list.json")
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            let products = data.responses[0][0].params.recommendedProducts['Size Özel']
+            console.log(products)
+            const html = products.map(product => {
+                return `
+                <div class='container'>
+                    <div class='row'>
+                        <div class="card col-md-3 text-break fs-small">
+                            <img src=${product.image} alt="Girl in a jacket" width="125" height="150" class="p-1 mx-auto mb-1">
+                            <p>${product.name}</p>
+                        </div>
+                    </div>
+                </div>
+                `
+            })
+            .join("")
+            document.querySelector('#main').insertAdjacentHTML("afterbegin", html)
+            console.log(html)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
 
 fetchData()
-
-
